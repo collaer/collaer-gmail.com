bbdd553554fff043a76e5f119f37facb232445e0
Julien Collaer <julien.collaer@opengeode.be>
2020-03-26 14:23:13 -0400

    v0.1 (#2)
    * Update index.html
    
    * Update README.md
    
    * Update csv2table.js
    
    * Update index.html
    
    * Update csv2table.js
    
    * Update index.html
    
    * Update csv2table.js
    
    * Update csv2table.js
    
    * Update csv2table.js
    
    * Update csv2table.js
    
    * Update csv2table.js
    
    * Update csv2table.js
    
    * Update csv2table.js
    
    * Update csv2table.js
    
    * Update csv2table.js
    
    * Update csv2table.js
    
    * Update csv2table.js
    
    * Update index.html
    
    * Update index.html
    
    * Update csv2table.js
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update csv2table.js
    
    * Update csv2table.js
    
    * Update csv2table.js
    
    * Update index.html
    
    * Update csv2table.js
    
    * Update csv2table.js
    
    * Update csv2table.js
    
    * Update csv2table.js
    
    * Update csv2table.js
    
    * Update csv2table.js
    
    * Update csv2table.js
    
    * Update csv2table.js
    
    * Update csv2table.js
    
    * Update csv2table.js
    
    * Add files via upload
    
    https://en.wikipedia.org/wiki/Coronavirus#/media/File:SARS-CoV-2_without_background.png
    
    * Delete 1280px-SARS-CoV-2_without_background.png
    
    * Add files via upload
    
    from original here
    https://en.wikipedia.org/wiki/Coronavirus#/media/File:SARS-CoV-2_without_background.png
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # csv2table
 
 Very simple porject: csv 2 bootstrap table js
+
+Demo here: https://collaer.github.io/csv2table/
+
+
+## Main open source lib used:
+
+* https://jquery.com/
+* https://getbootstrap.com/
+* https://bootstrap-table.com/docs/getting-started/usage/
+* https://github.com/typeiii/jquery-csv
--- csv2table.js
+++ csv2table.js
@@ -1,21 +1,155 @@
-$('#table').bootstrapTable({
-  columns: [{
-    field: 'id',
-    title: 'Item ID'
-  }, {
-    field: 'name',
-    title: 'Item Name'
-  }, {
-    field: 'price',
-    title: 'Item Price'
-  }],
-  data: [{
-    id: 1,
-    name: 'Item 1',
-    price: '$1'
-  }, {
-    id: 2,
-    name: 'Item 2',
-    price: '$2'
-  }]
-})
+var csv;
+var countries;
+var categories;
+
+String.prototype.trunc = String.prototype.trunc ||
+      function(n){
+          return (this.length > n) ? this.substr(0, n-1) + '&hellip;' : this;
+      };
+
+var processData=function(data) {
+  csv = $.csv.toObjects(data);
+	
+	
+  
+  $('#datatable').bootstrapTable({
+      pagination: true,
+      search: true,
+      pagination: true,
+      toolbar: "#toolbar-table",
+      showExport: true,
+      exportDataType: "all",
+      //showToggle: true,
+      showColumns: true,
+      showFullscreen: true,
+      showFooter: true,
+      detailView: true,
+      detailViewByClick: true,
+      detailViewIcon: true,
+      columns: [{
+        field: 'organization',
+        title: 'Organization',
+        width: '20',
+        widthUnit: '%',
+        sortable: true,
+        formatter: linkAccount,
+	footerFormatter: TotalLabelFormater
+      }, {
+        field: 'category',
+        title: 'Category',
+        width: '20',
+        widthUnit: '%',
+        sortable: true,
+	footerFormatter: TotalFormater
+      }, {
+        field: 'country',
+        title: 'Country',
+        width: '10',
+        widthUnit: '%',
+        sortable: true
+      }, {
+        field: 'description',
+        title: 'Description',
+        //cardVisible: true,
+        width: '40',
+        widthUnit: '%',
+        //visible: true,
+	sortable: false,
+	formatter: descriptionFormatter,
+	detailFormatter: detailDescriptionFormatter
+      }, {
+        field: 'url2',
+	title: 'Additional link',
+        width: '10',
+        widthUnit: '%',
+      }, {
+        field: 'url',
+	title: 'url',
+        width: '0.1',
+        widthUnit: '%',
+	visible: false
+      }],
+      data: csv
+  });
+  
+  countries = [];
+  categories = [];
+  $.each(csv, function(i,d) { 
+  	//console.log(d);
+	if(! countries.includes(d.country)) {
+	  countries.push(d.country);
+	  $("#country")
+   		.append('<option val="' + d.country + '">'+ d.country + '</option>');
+	}
+	if(! categories.includes(d.category)) {
+	  categories.push(d.category);
+	  $("#category")
+   		.append('<option val="' + d.category + '">'+ d.category + '</option>');
+	}
+  });
+	
+	$("#country").selectpicker('refresh');
+	$("#category").selectpicker('refresh');
+	
+	$( "#country" ).change(function() {
+		refreshFilter();
+	});
+
+	$( "#category" ).change(function() {
+	 	refreshFilter();
+	});
+};
+
+var refreshFilter = function() {
+	//console.log($("#country").val());
+	//console.log($("#category").val());
+	var country = $("#country").val();
+	var category = $("#category").val();
+	filters = {};
+	if (country != 0) {
+		filters["country"] = country;
+	};
+	if (category != 0) {
+		filters["category"] = category;
+	};
+	$('#datatable').bootstrapTable('filterBy', filters);
+	//console.log(filters);
+};
+
+var linkAccount = function (value, row, index) {
+	return [
+		'<a href="http://',
+		row.url,
+		'" title="Open webpage of ',
+		row.organization,
+		' in antoher window." target="_blank">',
+		value,
+		'</a>'].join('');
+};
+
+var TotalLabelFormater = function(data) {
+    return 'Total';
+};
+
+var TotalFormater = function(data) {
+    return data.length;
+ };
+
+var descriptionFormatter  = function (value, row, index) {
+	return value.trunc(133);
+};
+
+var detailDescriptionFormatter  = function (value, row, index) {
+	return row.description;
+};
+
+$(document).ready(function() {
+  $.ajax({
+        type: "GET",
+        url: "data/sample.csv",
+        dataType: "text",
+        success: function(data) {
+          processData(data);
+        }
+     });
+});
